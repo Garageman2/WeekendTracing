af8621a593e95e6389c6ca7783ec05529a72e917
Bailey <61953092+Garageman2@users.noreply.github.com>
2022-05-23 11:30:23 -0700

    AddedSphere

--- Src/main.cpp
+++ Src/main.cpp
@@ -4,8 +4,22 @@
 #include "Color.h"
 #include "Ray.h"
 
+bool HitSphere(const Point3& Center, double Radius, const Ray& r)
+{
+    Vec3 Oc = r.Origin() - Center;
+    auto a = dot(r.Direction(),r.Direction());
+    auto b =  2.0 * dot(Oc, r.Direction());
+    auto c = dot(Oc,Oc) - Radius*Radius;
+    auto Discriminant = b*b -4*a*c;
+    return (Discriminant > 0);
+}
+
 Color RayColor(const Ray& r)
 {
+    if(HitSphere(Point3(0,0,-1),0.5,r))
+    {
+        return Color(.5,.5,.65);
+    }
     Vec3 UnitDirection = UnitVector(r.Direction());
     auto t = 0.5*(UnitDirection.y()+1.0);
     return (1.0-t)*Color(1.0,1.0,1.0)+ t*Color(0.5,0.7,1.0);
@@ -15,7 +29,7 @@ int main() {
 
     //basics
     const auto AspectRatio = 16/9;
-    const int ImageWidth = 512;
+    const int ImageWidth = 1024;
     const int ImageHeight = static_cast<int>(ImageWidth / AspectRatio);
 
     //Camera
